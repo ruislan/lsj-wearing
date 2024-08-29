14e67c6fc4b08fb55bc225a8681ba4f251f3af18
ruislan <z17520@126.com>
2024-08-29 16:17:41 +0800

    fix dark

--- src/app/(frontend)/about/page.js
+++ src/app/(frontend)/about/page.js
@@ -4,7 +4,7 @@ export default async function Page() {
         <div className='w-full'>
             <div className='mx-8 max-w-2xl py-20 sm:mx-auto'>
                 <h1 className='mb-8 text-5xl font-bold'>About Us</h1>
-                <div className='prose mx-auto max-w-6xl text-base leading-7 text-black prose-headings:mt-8 prose-headings:font-semibold prose-headings:tracking-wide prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-a:text-black prose-a:underline hover:prose-a:text-neutral-300 prose-strong:text-black prose-ol:mt-8 prose-ol:list-decimal prose-ol:pl-6 prose-ul:mt-8 prose-ul:list-disc prose-ul:pl-6 dark:text-white dark:prose-headings:text-white dark:prose-a:text-white dark:prose-strong:text-white mb-8'>
+                <div className='prose mx-auto max-w-6xl text-base leading-7 prose-headings:mt-8 prose-headings:font-semibold prose-headings:tracking-wide prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-a:underline hover:prose-a:text-neutral-300 prose-ol:mt-8 prose-ol:list-decimal prose-ol:pl-6 prose-ul:mt-8 prose-ul:list-disc prose-ul:pl-6 text-white prose-headings:text-white prose-a:text-white prose-strong:text-white mb-8'>
                     <p>LingShengJi was founded in 2023 by Hanhao and Zhaorui.</p>
                     <p>As a hat supplier from China, we have a strong passion for the hat supply industry.</p>
                     <p>Our vision was to create a business that not only provides high-quality hats but also offers unique customization options.</p>
