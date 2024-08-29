118e734c56b2a6943b8d2352d79899a8b96bbb53
ruislan <z17520@126.com>
2024-08-29 16:00:46 +0800

    fix bug

--- src/app/(frontend)/contact/page.js
+++ src/app/(frontend)/contact/page.js
@@ -5,7 +5,7 @@ export default async function Page() {
             <div className='mx-8 max-w-3xl py-20 sm:mx-auto'>
                 <h1 className='mb-8 text-5xl font-bold'>Contact Us</h1>
                 <div className='prose mx-auto max-w-6xl text-base leading-7 text-black prose-headings:mt-8 prose-headings:font-semibold prose-headings:tracking-wide prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-a:text-black prose-a:underline hover:prose-a:text-neutral-300 prose-strong:text-black prose-ol:mt-8 prose-ol:list-decimal prose-ol:pl-6 prose-ul:mt-8 prose-ul:list-disc prose-ul:pl-6 dark:text-white dark:prose-headings:text-white dark:prose-a:text-white dark:prose-strong:text-white mb-8'>
-                    <p>If you have any questions regarding deliveries, materials, sizes or anything  – don't hesitate to contact us. We are happy to help you!</p>
+                    <p>If you have any questions regarding deliveries, materials, sizes or anything  – don&apos;t hesitate to contact us. We are happy to help you!</p>
                     <p>Please contact us in English and Chinese. The available time to contact us is from 10 am to 10 pm.</p>
                     <p>Email: <b>chongqinglingshengji@gmail.com</b>, <b>18810561953hh@gmail.com</b></p>
                     <p>Our goal is to respond within 48 hours if you reach out ot us via email.  If you contact us via email during the weekend or public holidays, we will get back to you as soon as possible on the next business day. </p>
